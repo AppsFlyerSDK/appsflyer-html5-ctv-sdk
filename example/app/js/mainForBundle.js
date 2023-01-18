17965c69688a078e26d44db35f5890361036c9ab
gabriel mandler <gabriel.mandler@appsflyer.com>
2023-01-18 11:55:10 +0200

    update main for bundle

--- example/app/js/mainForBundle.js
+++ example/app/js/mainForBundle.js
@@ -1,16 +1,16 @@
-const SAMSUNG_CONFIG = {
+var SAMSUNG_CONFIG = {
     devKey: "RxutGo4bSB9MKkM7bMCjHP",
     appId: "3202204027284",
     isDebug: true,    
     isSandbox: false
 };
-const LG_CONFIG = {
+var LG_CONFIG = {
     devKey: "Jtef5xTpBf9M3rXtRhU2Fd",
     appId: "27022015",
     isDebug: true,    
     isSandbox: false
 };
-const CUSTOM_CONFIG = {
+var CUSTOM_CONFIG = {
     devKey: "pJtNoWRvepn9EBtYG4jAUQ",
     appId: "12345678",
     isDebug: true,    
@@ -18,38 +18,40 @@ const CUSTOM_CONFIG = {
 }
 
 function main(){
-    let appsflyer = AppsFlyerSDK.getInstance();
+    var appsflyer = AppsFlyerSDK.getInstance();
 
-    let config = SAMSUNG_CONFIG;
-    // config = LG_CONFIG;
+    var config = SAMSUNG_CONFIG;
+    config = LG_CONFIG;
     // config = CUSTOM_CONFIG;
 
-    appsflyer.init(config).then((response)=>{
+    appsflyer.init(config).then(function(response){
         console.log("Init API response success: " + response);
-   
-        // let customPayload = {
-        //     "device_model": "3920X",
-        //     "device_os_version": "9.3.0",
-        //     "app_version": "1.0.5",
-        //     "device_id": "fa73d67d-f55c-5af3-883a-726253dc7d0e",
-        // }
+        var customPayload = {
+            "device_model": "3920X",
+            "device_os_version": "9.3.0",
+            "app_version": "1.0.5",
+            "device_id": "fa73d67d-f55c-5af3-883a-726253dc7d0e",
+        }
         // appsflyer.setCustomPayload(customPayload);
-
+        
         appsflyer.start()
-            .then((response)=>{
+            .then(function(response){
+                alert("3")
                 console.log("start API response success: " + JSON.stringify(response));
-            }).catch((err)=>{
+            }).catch(function(err){
+                alert("start failed " + JSON.stringify(err))
                 console.log("start API response err: " + JSON.stringify(err));
             });
 
-        appsflyer.logEvent("af_purchase", {"af_revenue" : 1.99, "af_currency": "USD"})
-                .then((response)=>{
-                    console.log("logEvent API response success: " + JSON.stringify(response));
-                }).catch((err)=>{
-                    console.log("logEvent API response err: " + JSON.stringify(err));
-                });    
+        // appsflyer.logEvent("af_purchase", {"af_revenue" : 1.99, "af_currency": "USD"})
+        //         .then(function(response){
+        //             console.log("logEvent API response success: " + JSON.stringify(response));
+        //         }).catch(function(err){
+        //             console.log("logEvent API response err: " + JSON.stringify(err));
+        //         });    
 
-    }).catch((err)=>{
+    }).catch(function(err){
+        alert("init err " + JSON.stringify(err))
         console.log("Init API response err: " + err);
     });
 }
